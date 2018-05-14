ad6c0c5f8e8151768c96f9e17c411b2b95390f49
Parin Thakkar <parin.theace@gmail.com>
2018-05-14 12:43:07 +0530

    project initialised

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,29 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+// require jquery
+// require jquery_ujs
+// require turbolinks
+// require_tree .
+//= require library/modernizr.custom.97074
+//= require library/jquery-1.11.3.min
+//= require bootstrap
+//= require jquery.easing.1.3
+//= require library/vegas.min
+//= require plugins
+//= require typed
+//= require fappear
+//= require jquery.countTo
+//= require owl.carousel
+//= require jquery.magnific-popup.min
+//= require SmoothScroll
+//= require common
